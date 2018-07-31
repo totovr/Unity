656355aef6a51b16126161b0fad6481b2aa6294e
Antonio Vega <a.vegaramirez76@gmail.com>
2018-07-31 23:05:47 +0900

    Timer

---
+++ Gun/Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    // variables of the class
+    public Text timerText;
+    private float startTime;
+    private float t = 0;
+
+	// Use this for initialization
+	void Start () {
+        // Initialize the timer
+        startTime = Time.time;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (t < 59.99)
+        {
+            // Timer until is displayed
+             t = Time.time - startTime;
+            // Calcute the time
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("f2");
+            // Display the time
+            timerText.text = minutes + ":" + seconds;
+            if (t > 44.99 && t < 59.99) {
+            timerText.color = Color.yellow;
+            //return;
+            } 
+        } else {
+            timerText.color = Color.red;
+            return;
+        }
+	}
+}
