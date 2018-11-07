729562ac5b7ce34ce0f8b9f0a9729c841ea5c54e
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-07 14:31:43 +0900

    Coins and timer
    add Coins with the logic of the game and a timer

---
+++ CoinsCollector/Assets/Scripts/Coin.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    // Static mean that it will be shared by all the instances of the class
+    public static int cointsCount = 0;
+
+	// Execute when the instance of this class is created
+	void Start ()
+    {
+        Debug.Log("A coin was created");
+        cointsCount++;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // An other collider has interact with this object
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            // Destroy the game object 
+            Destroy(gameObject);
+        }
+        
+    }
+
+    // OnDestroy works evertime that the gameobject was destroyed
+    void OnDestroy()
+    {
+        cointsCount--;
+
+        if(cointsCount <= 0)
+        {
+            Debug.Log("You have won the game");
+        }
+    }
+
+}
---
+++ CoinsCollector/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public float MaxTime = 60.0f;
+
+    private float countDown = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        countDown = MaxTime;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Time of rendering 
+        countDown -= Time.deltaTime;
+
+        if (countDown <= 0)
+        {
+            // Set the coins count to zero
+            Coin.cointsCount = 0;
+            // The game has end, load again the level
+            SceneManager.LoadScene("Level_01");
+            Debug.Log("Time is over");
+        }
+	}
+}
