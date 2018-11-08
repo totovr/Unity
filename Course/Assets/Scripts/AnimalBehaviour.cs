3d87eb463a0309fc8bd6d8878564c540c64a68c6
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-08 18:20:38 +0900

    The farm

---
+++ Course/Assets/Scripts/AnimalBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalBehaviour : MonoBehaviour {
+
+    public string animalSound = null;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        // Call the function that show display the animal sound
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            Sound();
+        }
+	}
+
+    void Sound()
+    {
+        Debug.Log(animalSound);
+    }
+}
