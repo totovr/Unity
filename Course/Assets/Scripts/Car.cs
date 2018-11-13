74f1043ac7530cfa7493af6dfbbc90a315db8b31
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-13 18:49:53 +0900

    class exercise

---
+++ Course/Assets/Scripts/Car.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    public string brand = "Mazda";
+    public string model = "Riata";
+    public Color color = Color.red;
+    public int hp = 160;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.A))
+        {
+            StartCar();
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            Stop();
+        }
+
+	}
+
+    void StartCar()
+    {
+        Debug.Log("Engine ON");
+    }
+
+    void Stop()
+    {
+        Debug.Log("The engine stop");
+    }
+}
---
+++ Course/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour {
+
+    public string playerName = "Unkown";
+    public int playerScore = 0;
+    public int playerAge = 0;
+
+    private int maxScore = 120;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(playerScore + 12);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.B))
+        {
+            BirthDay();
+        }
+	}
+
+    void BirthDay()
+    {
+        Debug.Log(playerAge + 1);
+    }
+}
