ec89303b8feecc12898313c7242357c0a62cfadd
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-16 14:15:13 +0900

    behaviour

---
+++ Course/Assets/Scripts/BehaviourTraining.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTraining : MonoBehaviour {
+
+    // Use this prior to initialization
+    void Awake () {
+        Debug.Log("The class is going to start");
+    }
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("The class start");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Print the actual time
+        Debug.Log(Time.time);
+    }
+}
+
