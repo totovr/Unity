8ec04a48d47acd6cf1cabeaca45d092d17efa9f3
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-21 01:10:07 +0900

    Change the color of an object

---
+++ Object_Light/Assets/Scripts/ColorBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorBehaviour : MonoBehaviour {
+
+    public Color myColor = Color.red;
+    MeshRenderer myRender;
+
+	// Use this for initialization
+	void Start () {
+
+        myRender = GetComponent<MeshRenderer>();
+        myRender.material.color = myColor;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
