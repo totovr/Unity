ea15cf70ec53688edfec33f669076ea284cc41de
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-30 19:06:47 +0900

    GameManager

---
+++ Bunny/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for start the game
+	public void StartGame () {
+		
+	}
+	
+    // Called when the player dies
+    public void GameOver ()
+    {
+
+    }
+
+    // Return to the main menu if the player dies 
+    public void BackToMainMenu()
+    {
+
+    }
+
+}
