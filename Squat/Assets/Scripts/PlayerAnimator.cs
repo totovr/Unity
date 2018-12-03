44bc00391eb3814744e1ada60c94d860af7de261
Antonio Vega <a.vegaramirez76@gmail.com>
2018-12-04 01:57:08 +0900

    Squat motion animation

---
+++ Squat/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour {
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("isActive", true);
+            animator.SetBool("isNotActive", true);     
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetBool("isActive", false);
+            animator.SetBool("isNotActive", false);
+        }
+    }
+}
