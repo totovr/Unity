b1d8a744f8a19587026bcb76f9b6f82a550db5e2
Antonio Vega <a.vegaramirez76@gmail.com>
2018-12-11 12:49:28 +0900

    FPS game

---
+++ Oculus/Shooting/Assets/Scripts/InputController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+    public AudioClip clip;
+    public AudioSource audioSource;
+
+    public Transform gunBarrelTransform;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            audioSource.Play();
+            RayCastGun();
+        }
+	}
+
+    void RayCastGun()
+    {
+        // Check the distance bettwen the object and the user
+        RaycastHit hit;
+        
+        if(Physics.Raycast(gunBarrelTransform.position, gunBarrelTransform.forward, out hit))
+        { 
+            if (hit.collider.gameObject.CompareTag("Enemy"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
