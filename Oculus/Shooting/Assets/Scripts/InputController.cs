5f9473f42ba01c663a89629d8554c60d32050517
Antonio Vega <a.vegaramirez76@gmail.com>
2018-12-13 01:35:58 +0900

    Improvevements
    - Animations
    - Sounds
    - Raycast

--- Oculus/Shooting/Assets/Scripts/InputController.cs
+++ Oculus/Shooting/Assets/Scripts/InputController.cs
@@ -3,38 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputController : MonoBehaviour {
-
-    public AudioClip clip;
-    public AudioSource audioSource;
-
-    public Transform gunBarrelTransform;
-
-	// Use this for initialization
-	void Start () {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = clip;
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
-        {
-            audioSource.Play();
-            RayCastGun();
-        }
+
+            if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                SoundController.sharedInstance.audioSource.Play();
+                ShootTrigger.sharedInstance.RayCastGun();
+            }
+
+        
 	}
 
-    void RayCastGun()
-    {
-        // Check the distance bettwen the object and the user
-        RaycastHit hit;
-        
-        if(Physics.Raycast(gunBarrelTransform.position, gunBarrelTransform.forward, out hit))
-        { 
-            if (hit.collider.gameObject.CompareTag("Enemy"))
-            {
-                Destroy(hit.collider.gameObject);
-            }
-        }
-    }
 }
