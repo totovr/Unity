ea85c6240b8e5c1cf74a9ad3d775662f9af34a04
Antonio Vega <a.vegaramirez76@gmail.com>
2019-02-15 12:04:40 +0900

    SkiPoleTracking
    Tracking the user hands and generate ski pole

---
+++ Nuitrack/SkiPoleTracking/Assets/Scripts/AnglesCalculation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+
+public class AnglesCalculation : MonoBehaviour {
+
+    public float AngleBetweenTwoVectors(Vector3 vectorA, Vector3 vectorB)
+    {
+        float dotProduct = 0.0f;
+        vectorA.Normalize();
+        vectorB.Normalize();
+        dotProduct = Vector3.Dot(vectorA, vectorB);
+
+        return (float)(Math.Acos(dotProduct)) / (float)Math.PI * 180.0f;
+    }
+}
