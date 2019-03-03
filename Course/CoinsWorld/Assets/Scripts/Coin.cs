50d82ae79bd7b356b5fd85fd7187a3bbe4a20de5
Antonio Vega <a.vegaramirez76@gmail.com>
2019-03-03 16:12:30 +0900

    coins prefabs created, material and behavior script

---
+++ Course/CoinsWorld/Assets/Scripts/Coin.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    public enum CoinType { BRONZE, SILVER, GOLD};
+
+    public CoinType Type;
+
+    private CoinType _type;
+
+    public Material[] CoinMaterials;
+
+    public static int CoinCounter = 0;
+
+    private int coinValue = 1;
+
+    CoinType coinMaterial
+    {
+
+        get
+        {
+            return _type;
+        }
+
+        set
+        {
+            _type = value;
+
+            int _typeValue = (int)_type;
+
+            Material _mat = CoinMaterials[_typeValue];
+
+            Renderer Rend = GetComponent<Renderer>();
+
+            if (Rend != null)
+            {
+                Rend.material = _mat;
+                coinValue = 1 + (int)Mathf.Pow(_typeValue, 2);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinMaterial = Type;
+        CoinCounter += coinValue;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    private void OnDestroy()
+    {
+        CoinCounter -= coinValue;
+
+        if (CoinCounter <= 0)
+        {
+            Debug.Log("All the coins were collected");
+        }
+
+    }
+
+
+}
